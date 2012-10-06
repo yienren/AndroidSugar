ba8d9fcefa4653bea5c2b52a506e9cb3ebdff526
Christopher Bowron <cwbowron@gmail.com>
2012-10-06 10:58:07 -0400

    getScreenCategory() and getScreenCategoryString()

--- src/com/bazquux/android/sugar/SystemInfo.java
+++ src/com/bazquux/android/sugar/SystemInfo.java
@@ -6,19 +6,25 @@ import android.content.res.Configuration;
 import android.util.DisplayMetrics;
 
 public class SystemInfo {
-    public static String getScreenCategory( Context context ) {
-        int screenSize  = context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK; 
-        if ( screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE ) {
-            return "xlarge";
-        } else if ( screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ) {
-            return "large";
-        } else if ( screenSize == Configuration.SCREENLAYOUT_SIZE_NORMAL ) {
-            return "normal";
-        } else if ( screenSize == Configuration.SCREENLAYOUT_SIZE_SMALL ) {
-            return "small";
-        }
-        return "unknown";
+
+    public static int getScreenCategory( Context context ) {
+        return context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK; 
      }
+
+    public static String getScreenCategoryString( Context context ) {
+        switch ( getScreenCategory( context ) ) {
+        case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+            return "xlarge";
+        case Configuration.SCREENLAYOUT_SIZE_LARGE:
+            return "large";
+        case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+            return "normal";
+        case Configuration.SCREENLAYOUT_SIZE_SMALL:
+            return "small";
+        default:
+            return "unknown";
+        }
+    }
     
     public static int getScreenWidth( Activity context ) {
         DisplayMetrics metrics = new DisplayMetrics();
