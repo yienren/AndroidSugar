3b7680abd6b490542a345d8e53fef64ffdcb684d
Christopher Bowron <cwbowron@gmail.com>
2012-10-13 10:20:04 -0400

    menus and preferences helpers

---
+++ src/com/bazquux/android/sugar/Menus.java
@@ -0,0 +1,47 @@
+package com.bazquux.android.sugar;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.MenuItem;
+
+public class Menus {
+    static public void setEnabledSafe( MenuItem item, boolean enabled ) {
+        if ( item != null ) {
+            item.setEnabled( enabled );
+        }
+    }
+    
+    static public void setIconSafe( MenuItem item, int resId ) {
+        if ( item != null ) {
+            item.setIcon( resId );
+        }
+    }
+    
+    static public void setIconColorFilterSafe( Context context, MenuItem menuItem, int resId, int color, PorterDuff.Mode filterMode ) {
+        if( menuItem != null ) {
+            Drawable d = context.getResources().getDrawable( resId );
+            d.mutate();
+            d.setColorFilter( 0xA000ff00, filterMode );
+            menuItem.setIcon( d );
+        }
+    }
+    
+    static public void setTitleSafe( MenuItem item, CharSequence title ) {
+        if ( item != null ) {
+            item.setTitle( title );
+        }
+    }
+    
+    static public void setVisibleSafe( MenuItem menuItem, boolean visible ) {
+        if ( menuItem != null ) {
+            menuItem.setVisible( visible );
+        }
+    }
+    
+    static public void setCheckedSafe( MenuItem menuItem, boolean checked ) {
+        if ( menuItem != null ) {
+            menuItem.setChecked( checked );
+        }
+    }
+}
---
+++ src/com/bazquux/android/sugar/SugarPreferences.java
@@ -0,0 +1,30 @@
+package com.bazquux.android.sugar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SugarPreferences {
+    public static void putInt( Context context, String id, String key, int value ) {
+        SharedPreferences settings      = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt( key, value );
+        editor.apply();
+    }
+    
+    public static int getInt( Context context, String id, String key, int defaultValue ) {
+        SharedPreferences settings = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        return settings.getInt( key, defaultValue );
+    }
+
+    public static void putBoolean( Context context, String id, String key, boolean value ) {
+        SharedPreferences settings      = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean( key, value );
+        editor.apply();
+    }
+    
+    public static boolean getBoolean( Context context, String id, String key, boolean defaultValue ) {
+        SharedPreferences settings = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        return settings.getBoolean( key, defaultValue );
+    }
+}
