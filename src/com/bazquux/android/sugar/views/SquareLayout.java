4ee26d8448f547566a0aa998d0b216a6b76b79d3
Christopher Bowron <cwbowron@gmail.com>
2012-10-15 19:21:04 -0400

    sqaure layout from stackoverflow

---
+++ src/com/bazquux/android/sugar/views/SquareLayout.java
@@ -0,0 +1,33 @@
+package com.bazquux.android.sugar.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SquareLayout extends LinearLayout {
+
+    double mScale = 1.00;
+    
+    public SquareLayout( Context context ) {
+        super( context );
+    }
+
+    public SquareLayout( Context context, AttributeSet attrs ) {
+        super( context, attrs );
+    }
+
+    @Override
+    protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
+
+        int width = MeasureSpec.getSize( widthMeasureSpec );
+        int height = MeasureSpec.getSize( heightMeasureSpec );
+
+        if ( width > (int) ( mScale * height + 0.5 ) ) {
+            width = (int) ( mScale * height + 0.5 );
+        } else {
+            height = (int) ( width / mScale + 0.5 );
+        }
+
+        super.onMeasure( MeasureSpec.makeMeasureSpec( width, MeasureSpec.EXACTLY ), MeasureSpec.makeMeasureSpec( height, MeasureSpec.EXACTLY ) );
+    }
+}
