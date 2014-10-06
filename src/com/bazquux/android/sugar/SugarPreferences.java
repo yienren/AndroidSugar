8c44d4d465ce6687a0cf366e8762752ba7254dc9
cwbowron <cwbowron@gmail.com>
2014-10-06 15:43:53 -0400

    Add putString, getString and putLong

--- src/com/bazquux/android/sugar/SugarPreferences.java
+++ src/com/bazquux/android/sugar/SugarPreferences.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SugarPreferences {
+
+    public static void putLong( Context context, String id, String key, long value ) {
+        SharedPreferences settings      = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong( key, value );
+        editor.apply();
+    }
+    
     public static void putInt( Context context, String id, String key, int value ) {
         SharedPreferences settings      = context.getSharedPreferences( id, Context.MODE_PRIVATE );
         SharedPreferences.Editor editor = settings.edit();
@@ -26,5 +34,17 @@ public class SugarPreferences {
     public static boolean getBoolean( Context context, String id, String key, boolean defaultValue ) {
         SharedPreferences settings = context.getSharedPreferences( id, Context.MODE_PRIVATE );
         return settings.getBoolean( key, defaultValue );
+    }
+
+    public static void putString( Context context, String id, String key, String value ) {
+        SharedPreferences settings      = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString( key, value );
+        editor.apply();
+    }
+    
+    public static String getString( Context context, String id, String key, String defaultValue ) {
+        SharedPreferences settings = context.getSharedPreferences( id, Context.MODE_PRIVATE );
+        return settings.getString( key, defaultValue );
     }
 }
